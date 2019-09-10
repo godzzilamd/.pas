b747d2e40644d87983842ea59910945f0d48250d
godzzilamd <godzzilamd@mail.ru>
2019-09-10 19:29:34 +0300

    while

---
+++ laborator4/while.pas
@@ -0,0 +1,17 @@
+Program P4;
+Uses Crt;
+
+var x: single= -3;
+
+Begin
+ClrScr;
+while (x <= 3) do
+begin
+    write(x * x:1:1, '   ');
+    x := x + 0.5;
+end;
+
+writeln;
+writeln('Tastati orice pentru a iesi');
+readkey;
+end.
