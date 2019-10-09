f4e784c456a7c762dc3cc8db0986afb9dd013d5d
godzzilamd <mishanea@herewai.zte.com.cn>
2019-10-09 21:03:30 +0300

    UST

--- laborator6/ex6_p10_sus.pas
+++ laborator6/ex6_p10_sus.pas
@@ -1,18 +1,34 @@
 Program P1;
+Uses Crt;
 
-var x,n,i,j:byte;
-    s:integer;
+var x,n,i,j,k:byte;
+    Permutari : byte = 1;
     a : array [0 .. 100] of byte;
 
 begin
+ClrScr;
+//introducerea datelor
 write('n = '); readln(n);
 write('x = '); readln(x);
-for i := 0 to n do
+writeln('Valorile cercurilor: ');
+for i := 1 to n do
+    read(a[i]);
+
+//calcularea permutarilor
+for i := 1 to n do
+    Permutari := Permutari * i;
+
+//backtracking fara functii si filtrare
+for i := 1 to n do
 begin
-    while (s <> n) do
+    for j := 1 to n do
     begin
-        
-    end;    
+        for k := 1 to n do
+        begin
+            if a[i] + a[j] + a[k] = x then writeln(a[i], ' ', a[j], ' ', a[k]);
+        end;
+    end;
 end;
+
 readln;
 end.
