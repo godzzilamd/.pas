10d3512075a91e278852fc82637b1047d0786902
godzzilamd <mishanea@herewai.zte.com.cn>
2019-10-16 20:25:56 +0300

    UST

---
+++ test2/ex7_p14.pas
@@ -0,0 +1,25 @@
+Program P1;
+Uses Crt;
+
+var n,m,j,i:byte;
+    a : array [1 .. 100, 1 .. 100] of byte;
+    s : byte = 0;
+
+begin
+ClrScr;
+write('n = '); readln(n);
+write('m = '); readln(m);
+
+for i := 1 to n do
+    for j := 1 to m do
+        read(a[i][j]);
+
+for i := 1 to n do
+begin
+    for j := 1 to m do
+    s := s + a[j][i];
+    writeln('Suma pe ', i, ' rind este : ', s);
+end;
+
+readkey;
+end.
